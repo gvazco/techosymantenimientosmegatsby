1a08e4d2561d41b1efa8b8a95d321673e3dfbde0
Gustavo Vázco <gustavovazco@gmail.com>
2023-08-01 16:18:07 -0600

    📦 NEW: add Image Gallery component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 import "./src/styles/global.css";
 import "./public/themeStylesheet.css";
 import "@webdeveducation/wp-block-tools/dist/css/style.css";
+import "react-image-gallery/styles/css/image-gallery.css";
---
+++ src/components/ImagesGallery/ImagesGallery.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+
+export const ImagesGallery = ({ children }) => {
+  const content = children.props.blocks;
+  console.log(content);
+
+  const imageItem = content.map((item, index) => (
+    <div class="relative transform rounded-lg border-4 border-slate-800 shadow-md transition duration-500 hover:scale-105">
+      <div class="flex justify-center">
+        <a href="#">
+          <GatsbyImage
+            key={index}
+            alt={item.id}
+            image={item.attributes.gatsbyImage}
+          />
+        </a>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="alignwide are-vertically-aligned-center mb-2">
+      <div class="grid grid-cols-1 gap-5 p-3 sm:grid-cols-2 md:grid-cols-3 md:p-2">
+        {imageItem}
+      </div>
+    </div>
+  );
+};
