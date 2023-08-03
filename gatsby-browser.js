4b17cd62a3664de59e8d9946a2db80238fddf047
Gustavo Vázco <gustavovazco@gmail.com>
2023-08-03 03:56:22 -0600

    📦 NEW: add vercel analitycs plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,5 @@ import "./src/styles/global.css";
 import "./public/themeStylesheet.css";
 import "@webdeveducation/wp-block-tools/dist/css/style.css";
 import "react-image-gallery/styles/css/image-gallery.css";
+import { inject } from '@vercel/analytics';
+inject();
