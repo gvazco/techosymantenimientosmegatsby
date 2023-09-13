1383133c13658ba7ab6387aea6ed0757bde39a40
Gustavo Vázco <gustavovazco@gmail.com>
2023-09-12 21:07:15 -0600

    🐛 FIX: manifest icon name

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
       options: {
         start_url: `/`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
